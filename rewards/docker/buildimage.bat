600aef2e49e8b1e90b0dcf140054184244a6da99
siridavuluri <31948054+siridavuluri@users.noreply.github.com>
2023-01-12 11:57:19 -0500

    Delete rewards directory

--- rewards/docker/buildimage.bat
+++
@@ -1,4 +0,0 @@
-docker stop reward
-docker rm reward
-docker rmi -f reward:latest
-docker build ./.. --file=dockerfile --tag=reward
--- rewards/src/main/java/com/test/rewards/exceptions/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.test.rewards.exceptions;
-
-public class BusinessException extends Exception {
-    public BusinessException(String cause) {
-        super(cause);
-    }
-}
